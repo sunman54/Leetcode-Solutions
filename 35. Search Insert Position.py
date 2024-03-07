c43b49f801e20746c85db19880ff09311e64ebb2
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-03-08 00:35:50 +0300

    Create 35. Search Insert Position.py

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        nums.append(target)
+
+        nums.sort()
+
+        return nums.index(target)
+        
