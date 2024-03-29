61177e3233a16c0438d39c0f6e4157aa3044e1ff
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-03-29 16:21:26 +0300

    Problem Solved : 14

--- 14. Longest Common Prefix/app.py
+++ 14. Longest Common Prefix/app.py
@@ -17,3 +17,17 @@ class Solution:
 
             except:
                 return prefix
+            
+
+        """ best solution
+        class Solution:
+            def longestCommonPrefix(self, strs: List[str]) -> str:
+                for i in range(len(strs[0])):
+                    c = strs[0][i]
+                    print(c)
+                    for s in strs[1:]:
+                        if len(s) == i or s[i] != c:
+                            return strs[0][0:i]
+                return strs[0]
+        
+        """
